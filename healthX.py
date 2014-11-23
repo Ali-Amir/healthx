2259cf7137ff448dc9410a2fe283ca9d28e2b4c4
Ali-Amir Aldan <aliamir@mit.edu>
2014-11-22 23:06:12 -0500

    Now the server returns response

--- healthX.py
+++ healthX.py
@@ -5,3 +5,9 @@ class HealthXHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
   def do_GET(self):
     parsed_path = urlparse.urlparse(self.path)
+    print parsed_path
+    self.send_response(200)
+    self.send_header('content-type', 'text/html')
+    self.end_headers()
+    self.wfile.write('Shit')
+
